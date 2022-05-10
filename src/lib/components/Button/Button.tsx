270e2fe9d59351d6916955c40add9a0f618028a1
LAPTOP-G3BBDCB7\ASUS <thawatchai.ng@inet.co.th>
2022-05-10 10:18:49 +0700

    [Edited]: button componant

--- src/lib/components/Button/Button.tsx
+++ src/lib/components/Button/Button.tsx
@@ -7,11 +7,7 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = (props) => {
-<<<<<<< HEAD
-  return <button className={props?.type === "primary" ? "e-btn-success" : ""}>{props?.text}</button>;
-=======
   return <button onClick={props?.onClick} className={ props?.type === "primary" ? "e-btn e-btn-primary" : "e-btn"}>{props?.text}</button>;
->>>>>>> 489ba598c05406ae3bc32aa0634e8f23ab9a91f7
 };
 
 export default Button;
